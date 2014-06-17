1182b51bd9895c0e8ef605dfaadf3fd48d7338b0
Adam M Rivera <arivera@sovereignshare.com>
2014-06-16 20:52:13 -0500

    Fixed target block timespan.

--- BreadWallet/BRMerkleBlock.m
+++ BreadWallet/BRMerkleBlock.m
@@ -31,7 +31,7 @@
 
 #define MAX_TIME_DRIFT    (2*60*60)     // the furthest in the future a block is allowed to be timestamped
 #define MAX_PROOF_OF_WORK 0x1d00ffffu   // highest value for difficulty target (higher values are less difficult)
-#define TARGET_TIMESPAN   (14*24*60*60) // the targeted timespan between difficulty target adjustments
+#define TARGET_TIMESPAN   (2.5*60) 		// the targeted timespan between difficulty target adjustments (2.5 min)
 
 // convert difficulty target format to bignum, as per: https://github.com/bitcoin/bitcoin/blob/master/src/uint256.h#L506
 static void setCompact(BIGNUM *bn, uint32_t compact)
