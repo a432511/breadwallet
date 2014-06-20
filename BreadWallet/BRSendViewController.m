ed8c2bee61a4f6b23dfb4b47d3aab8f117d9dc1c
Juraj Petrik <juraj.petrik@taqtile.com>
2014-06-19 23:56:19 -0300

    commit that breaks the build, will travis fail too?

--- BreadWallet/BRSendViewController.m
+++ BreadWallet/BRSendViewController.m
@@ -23,7 +23,7 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 //  THE SOFTWARE.
 
-
+nonsense
 #import "BRSendViewController.h"
 #import "BRRootViewController.h"
 #import "BRAmountViewController.h"
