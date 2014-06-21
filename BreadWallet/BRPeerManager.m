b3ed720d6853345caff8aee8a7a75fe1c9fffd3e
sovereignshare <a432511@gmail.com>
2014-06-20 23:16:42 -0500

    Fixed merge issue.

--- BreadWallet/BRPeerManager.m
+++ BreadWallet/BRPeerManager.m
@@ -23777,7 +23777,7 @@ static const char *dns_seeds[] = {
     }
 
     if (_blocks.count > 0) {
-        if (blockHeight >= self.lastBlockHeight - BLOCK_DIFFICULTY_INTERVAL*2) { // recent block we have the header for
+        if (blockHeight >= self.lastBlockHeight - blockDifficultyInterval*2) { // recent block we have the header for
             BRMerkleBlock *block = self.lastBlock;
 
             while (block && block.height > blockHeight) {
